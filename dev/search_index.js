c986e997445159e4ff4c8f63824f0e2bfef8ce7f
Documenter.jl <documenter@juliadocs.github.io>
2024-05-24 07:52:33 +0000

    build based on b12a96c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkHypertextTools","category":"page"},{"location":"#OkHypertextTools","page":"Home","title":"OkHypertextTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkHypertextTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkHypertextTools]","category":"page"}]
+}
